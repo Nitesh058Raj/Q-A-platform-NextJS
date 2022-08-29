b5b699bc37ac4e4a593cdd0ad243404c1a55dbb8
Nitesh <05niteshraj@gmail.com>
2022-08-30 00:34:37 +0530

    small changes in css

--- components/footer.js
+++ components/footer.js
@@ -2,5 +2,9 @@ import Link from "next/link";
 import styles from "./footer.module.css";
 
 export default function Footer() {
-  return <h1 className={styles.h1text}>Footer</h1>;
+  return (
+    <div className={styles.contain}>
+      <h1 className={styles.h1text}>Footer</h1>
+    </div>
+  );
 }
